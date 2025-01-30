a94fae597cb42afdd853b601e23875caab607671
Brenno Santos <devblsds@gmail.com>
2025-01-30 14:36:42 -0300

    Create function to update friends list

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 let amigos = [];
 
+function atualizarLista() {
+  let listaAmigos = document.getElementById('listaAmigos');
+  listaAmigos.innerHTML = '';
+
+  for (let i = 0; i < amigos.length; i++) {
+    let amigo = amigos[i];
+
+    let item = document.createElement('li');
+    item.innerText = amigo;
+    listaAmigos.appendChild(item);
+  }
+}
+
 function adicionarAmigo() {
   let nome = document.getElementById('amigo');
 
-  if (nome == '') {
+  if (nome.value == '') {
     alert('Por favor, insira um nome.')
   } else {
-    amigos.push(nome);
+    amigos.push(nome.value);
     nome.value = '';
+    atualizarLista();
   }
 }
