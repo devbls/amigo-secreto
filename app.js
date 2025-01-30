439ef1137b4f7448ca140f5fe11baa7493d1e949
Brenno Santos <devblsds@gmail.com>
2025-01-30 14:18:14 -0300

    Create function to add friends

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let nome = document.getElementById('amigo');
+
+  if (nome == '') {
+    alert('Por favor, insira um nome.')
+  } else {
+    amigos.push(nome);
+    nome.value = '';
+  }
+}
